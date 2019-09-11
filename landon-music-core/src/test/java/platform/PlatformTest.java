4124dcc0fa11436666935dd635ee981a164ae707
John Caridi <John.Caridi@openlink.com>
2019-09-11 17:05:50 -0400

    Adding test

--- landon-music-core/src/test/java/platform/PlatformTest.java
+++ landon-music-core/src/test/java/platform/PlatformTest.java
@@ -1,4 +1,12 @@
 package platform;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PlatformTest {
+
+    @Test
+    public void testPlatform(){
+        Assert.assertTrue(true);
+    }
 }
